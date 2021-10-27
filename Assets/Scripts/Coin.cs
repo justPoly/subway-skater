edea5eee2a4ba6f130474d5d46dfbeff9443b33e
Ehize <polycarpatalor@gmail.com>
2021-10-27 19:53:01 +0100

    subway skater files upload

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+       anim = GetComponent<Animator>(); 
+    }
+
+    private void OnEnable()
+    {
+       anim.SetTrigger("Spawn");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.tag == "Player")
+       {
+           GameManager.Instance.GetCoin();
+           anim.SetTrigger("Collected");
+           Destroy(this.gameObject);
+       }
+    }
+}
